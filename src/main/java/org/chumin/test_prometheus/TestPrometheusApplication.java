fd7ac24e8187875744064a5f0d5ae8b67a0cb719
Oleg.Chumin <oleg.tschumin@gmail.com>
2025-04-08 13:53:16 +0300

    Add Prometheus, Grafana

--- src/main/java/org/chumin/test_prometheus/TestPrometheusApplication.java
+++ src/main/java/org/chumin/test_prometheus/TestPrometheusApplication.java
@@ -1,13 +1,27 @@
 package org.chumin.test_prometheus;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TestPrometheusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestPrometheusApplication.class, args);
+    }
+
+    @PostConstruct
+    public void onStart() {
+        log.info("Application started successfully!");
+    }
+
+    @PreDestroy
+    public void onShutDown() throws Exception {
+        log.info("Application shut down successfully!");
     }
 
 }
